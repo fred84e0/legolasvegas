7503350d046653dee77d2f2fe7c2b365ca5bb73a
Morgan Grage Rasmussen <morgangr01@gmail.com>
2019-11-29 12:45:17 +0100

    Morgans changes

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 console.log("haasd");
-console.log(fetchSVG);
 
 fetchSVG();
 
 function fetchSVG() {
-  console.log("hallooo");
-  fetch("svg/slotmachine.svg")
-    .then(response => response.text())
-    .then(svg => {
-      document.querySelector("#slotmachine").innerHTML = svg;
-      clickColor();
-    });
+  console.log();
 }
 
 function clickColor() {}
+
+window.addEventListener("DOMContentLoaded", event => {
+  fetch("svg/slotmachine.svg")
+    .then(response => response.text())
+    .then(svg => {
+      console.log(svg);
+      document.querySelector("#slotmachine").innerHTML = svg;
+      clickColor();
+    });
+});
